896bf9a461c43b8c0c34180918c5844c095f355e
junya takayama <nekonaderi@gmail.com>
2019-06-02 01:49:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+require('dotenv').config()
+const line = require('@line/bot-sdk')
+const puppeteer = require('puppeteer')
+
+// Line botのトークン
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+}
+
+const client = new line.Client(config)
+
+function handleEvent(event) {
+  console.log(event)
+  var message = 'Please push text message.'
+
+  if (event.type === 'message' && event.message.type === 'text') {
+    message = event.message.text
+  }
+
+  const echo = { type: 'text', text: message }
+  return client.replyMessage(event.replyToken, echo)
+}
+
+exports.handler = function echoBot(req, res) {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.status(200).send(`Success: ${result}`))
+    .catch(err => res.status(400).send(err.toString()))
+}
